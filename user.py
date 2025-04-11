f8215b923916c0092aa36f222fe9e8329db7f590
Iivari Anttila <iivari03@gmail.com>
2025-04-11 22:06:41 +0300

    Created user class

---
+++ user.py
@@ -0,0 +1,20 @@
+# File name: user.py
+# Description: A class for a user
+
+class User:
+    def __init__(self, id:int, first_name:str, learning_progresses:list):
+        self.__id = id
+        self.__first_name = first_name
+        self.__learning_progresses:list = learning_progresses
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def first_name(self):
+        return  self.__first_name
+
+    @property
+    def learning_progresses(self):
+        return self.__learning_progresses

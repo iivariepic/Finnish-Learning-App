72991029ab6d42122010692f793fa675e5d6bbf4
Iivari Anttila <iivari03@gmail.com>
2025-04-12 17:18:55 +0300

    Added part of speech possibilities

--- word/conjugation.py
+++ word/conjugation.py
@@ -2,9 +2,20 @@
 # Author: Iivari Anttila
 # Description: Conjugation for a word
 class Conjugation:
-    def __init__(self, finnish_translation:str, type:str):
+    def __init__(self,
+                 finnish_translation:str,
+                 type:str,
+                 comparison_degree:str = None,
+                 tense:str = None
+    ):
         self.__finnish_translation = finnish_translation
-        self.__type = type # Type means something like ("Third person", or "Inessive")
+        # Type means something like ("Third person", or "Inessive")
+        self.__type = type
+        # Comparison Degree is only for ajectives and means something like ("comparative", or "superlative")
+        self.__comparison_degree = comparison_degree
+        # Tense is only for verbs and means something like ("past", "present")
+        self.__tense = tense
+
 
     @property
     def finnish_translation(self):
@@ -13,3 +24,13 @@ class Conjugation:
     @property
     def type(self):
         return self.__type
+
+    @property
+    def comparison_degree(self):
+        if self.__comparison_degree is not None:
+            return self.__comparison_degree
+
+    @property
+    def tense(self):
+        if self.__tense is not None:
+            return self.__tense
--- word/word.py
+++ word/word.py
@@ -8,13 +8,20 @@ class Word(Target):
     def __init__(self,
                  english_translations:list[str],
                  finnish_translations:list[str],
-                 initial_conjugations:list[Conjugation]):
+                 part_of_speech:str,
+                 initial_conjugations:list[Conjugation] = []):
+
         super().__init__(english_translations, finnish_translations)
-        self.__conjugations = []
+        self.__part_of_speech = part_of_speech
+        self.__conjugations = initial_conjugations
 
     @property
     def conjugations(self):
         return self.__conjugations
+
+    @property
+    def part_of_speech(self):
+        return self.__part_of_speech
 
     def add_conjugation(self, conjugation:Conjugation):
         if conjugation.type not in self.conjugations:

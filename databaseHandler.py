36454a17e054943a26c5497445bb39a3b3427510
Iivari Anttila <iivari03@gmail.com>
2025-04-12 18:16:13 +0300

    Modified DatabaseHandler Class

--- databaseHandler.py
+++ databaseHandler.py
@@ -1,20 +1,54 @@
+# File name: databaseHandler.py
+# Author: Iivari Anttila
+# Description: A class for handling the database, all database interactions go through this class
 import sqlite3
 
-
 class DatabaseHandler:
-    def __init__(self):
-        self.database = "Finnish_Language_App.sqlite3"
+    def __init__(self, database_name = "Finnish_Language_App.sqlite3"):
+        self.__database = database_name
+        self.__connection = None
+        self.__cursor = None
+
+    @property
+    def database(self):
+        return self.__database
+
+    @property
+    def connection(self):
+        return self.__connection
+
+    @property
+    def cursor(self):
+        return self.__cursor
+
+    def __connect(self):
+        try:
+            self.__connection = sqlite3.connect(self.database)
+            self.__cursor = connection.cursor()
+        except sqlite3.Error as error:
+            print(f"Database Connection Error: {error}")
 
     def __execute_query(self, query:str):
-        connection = sqlite3.connect(self.database)
-        cursor = connection.cursor()
-        cursor.execute(query)
-        rows = cursor.fetchall()
+        if self.cursor:
+            try:
+                self.cursor.execute(query)
 
-        # Commit only methods that are not SELECT
-        if not query.upper().startswith("SELECT", 0, 30):
-            cursor.commit()
+                # Return rows if the QUERY was SELECT
+                if query.upper().startswith("SELECT", 0, 30):
+                    rows = cursor.fetchall()
+                    return rows
 
-        connection.close()
+                # Commit only queries that are not SELECT
+                else:
+                    cursor.commit()
 
-        return rows
+            except sqlite3.Error as error:
+                print(f"Query Execution Error: {error}")
+
+        else:
+            print("No connection established with the database")
+
+    def __close_connection(self):
+        if self.connection:
+            self.__connection.close()
+            self.__cursor = None
